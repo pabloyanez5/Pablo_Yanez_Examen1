590b7b85e0146f887fb416fa3e7acc05ca132a8e
asd <130101518+pabloyanez5@users.noreply.github.com>
2024-10-30 08:52:55 -0500

    Commit 2
    Creacion de Modelos

---
+++ Pablo_Yanez_Examen1/Models/Celular.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pablo_Yanez_Examen1.Models
+{
+    public class Celular
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string Modelo { get; set; }
+        [Range(2000,2024)]
+        public int Año { get; set; }
+        [Range(100,3000)]
+        public decimal Precio { get; set; }
+        public int PYanezId { get; set; }
+        [ForeignKey("PYanezId")]
+        public PYanez pYanez { get; set; }
+
+    }
+}
--- Pablo_Yanez_Examen1/Models/PYanez.cs
+++ Pablo_Yanez_Examen1/Models/PYanez.cs
@@ -1,9 +1,23 @@
-﻿namespace Pablo_Yanez_Examen1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pablo_Yanez_Examen1.Models
 {
     public class PYanez
     {
+        [Key]
         public int Id { get; set; }
-
+        [Required]
+        [MaxLength(150)]
         public string Name { get; set; }
+        [Range(18, 100)]
+        public int Edad { get; set; }
+        [Range(460, 100000)]
+        public decimal Sueldo { get; set; }
+        [Required]
+        public bool Estado { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Registro { get; set; }
+        public Celular celular { get; set; }
+
     }
 }
